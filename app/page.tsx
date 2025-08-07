5bcbe5f94a61078db3d6b8faf4db2aabbb71b9d9
gabriel-gga <gguimaraesabrao@gmail.com>
2025-08-07 14:01:57 -0300

    Finish about styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { AnimatedContainer } from "@/components/animated-container";
 import StackList from "@/components/stack-list";
+import { Info } from "lucide-react";
 
 export default function AboutPage() {
   return (
@@ -9,7 +10,12 @@ export default function AboutPage() {
           <h2 className="text-lg text-center font-semibold mb-2 sm:text-left lg:text-2xl">About</h2>
           <p className="mb-3 lg:mb-5">This project serves as my personal knowledge base for Next.js. Here, I document insights, tests, and lessons learned from hands-on experience, courses, and official documentation.</p>
           <p className="mb-3 lg:mb-5">Summarizing key concepts helps me quickly review important topics and make informed decisions during development.</p>
+          <p className="mb-2 lg:mb-4">The stack I have been using is:</p>
           <StackList />
+          <div className="flex gap-1.5 items-center lg:mt-8">
+            <Info className="text-primary" />
+            <span className="text-base font-medium italic">Click each topic for more details</span>
+          </div>
         </AnimatedContainer>
       </div>
     </main >
--- components/stack-list.tsx
+++ components/stack-list.tsx
@@ -7,21 +7,17 @@ import {
 export default function StackList() {
     return (
         <ul>
-            <div className="mb-2 lg:mb-4">
-                <p>The stack I have been using is: <span className="hidden sm:inline-block text-sm italic font-semibold">(click the topics for more details)</span></p>
-                <p className="text-xs italic font-semibold sm:hidden">(click the topics for more details)</p>
-            </div>
             <li className="mb-2 lg:mb-4">
                 <Popover>
                     <PopoverTrigger className="flex items-center gap-1 hover:cursor-pointer">
-                        <span>👉</span>
+                        <span>🔸</span>
                         <span className="hover:underline">Next.js</span>
                     </PopoverTrigger>
                     <PopoverContent align="start" className="w-80 sm:w-96 lg:w-full">
                         <div className="flex flex-col gap-2 text-sm sm:text-base">
-                            <p>👍 Allows developers to build full-stack applications using React</p>
-                            <p>👍 Leverages many advanced React features such as React Server Components and Suspense</p>
-                            <p>👍 Gives us many optimizations out of the box</p>
+                            <p>🚀 Allows developers to build full-stack applications using React</p>
+                            <p>🚀 Leverages many advanced React features such as React Server Components and Suspense</p>
+                            <p>🚀 Gives us many optimizations out of the box</p>
                         </div>
                     </PopoverContent>
                 </Popover>
@@ -29,28 +25,28 @@ export default function StackList() {
             <li className="mb-2 lg:mb-4">
                 <Popover>
                     <PopoverTrigger className="flex items-center gap-1 hover:cursor-pointer">
-                        <span>👉</span>
+                        <span>🔸</span>
                         <span className="hover:underline">Shadcn/ui</span>
                     </PopoverTrigger>
                     <PopoverContent align="start" className="w-80 sm:w-96 lg:w-full">
                         <div className="flex flex-col gap-2 text-sm sm:text-base">
-                            <p>👍 Open Source</p>
-                            <p>👍 Uses Tailwind CSS</p>
-                            <p>👍 Deeply integrated with Vercel ecossystem</p>
+                            <p>🚀 Open Source</p>
+                            <p>🚀 Uses Tailwind CSS</p>
+                            <p>🚀 Deeply integrated with Vercel ecossystem</p>
                         </div>
                     </PopoverContent>
                 </Popover>
             </li>
-            <li className="mb-2">
+            <li>
                 <Popover>
                     <PopoverTrigger className="flex items-center gap-1 hover:cursor-pointer">
-                        <span>👉</span>
+                        <span>🔸</span>
                         <span className="hover:underline">Supabase</span>
                     </PopoverTrigger>
                     <PopoverContent align="start" className="w-80 sm:w-96 lg:w-full">
                         <div className="flex flex-col gap-2 text-sm sm:text-base">
-                            <p>👍 Good free tier plan</p>
-                            <p>👍 Easy setup with Next.js and Shadcn</p>
+                            <p>🚀 Good free tier plan</p>
+                            <p>🚀 Easy setup with Next.js and Shadcn</p>
                         </div>
                     </PopoverContent>
                 </Popover>
