aef88454dd3a31cd5887134273221b4c0520c13a
gabriel-gga <gguimaraesabrao@gmail.com>
2025-08-07 08:13:32 -0300

    Improve not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,19 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { FileQuestionMarkIcon } from 'lucide-react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
+    const router = useRouter();
+
     return (
         <main className="flex h-full flex-col items-center justify-center gap-2 py-10">
             <FileQuestionMarkIcon className="w-10 text-gray-400" />
             <h2 className="text-xl font-semibold">404 Not Found</h2>
             <p>Could not find the requested page.</p>
-            <Button asChild>
-                <Link href="/">Go Back</Link>
+            <Button onClick={() => router.back()}>
+                Go Back
             </Button>
         </main>
     );
