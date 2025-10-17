e4b3aafa1f6e2aefdda485508f9ecfadbb074ee6
gabriel-gga <gguimaraesabrao@gmail.com>
2025-10-17 10:35:23 -0300

    Improve text

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
       <div className="flex flex-col pt-4 lg:pt-12">
         <AnimatedContainer>
           <h2 className="text-lg text-center font-semibold mb-2 sm:text-left lg:text-2xl">About</h2>
-          <p className="mb-4 lg:mb-5">This project provides notes and practical examples designed to help beginners understand the main concepts of Next.js.</p>
+          <p className="mb-4 lg:mb-5">This project provides notes and practical examples designed to help beginners understand some of the main concepts of Next.js.</p>
           <p className="mb-4 lg:mb-5">It also offers tips to speed up development, based on my hands-on experience, courses, and the official documentation.</p>
           <p className="mb-3 lg:mb-4">The stack I have been using in my latest projects is:</p>
           <StackList />
